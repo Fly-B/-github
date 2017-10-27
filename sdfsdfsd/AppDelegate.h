0f68e9c1103364f10bb28a9f77b2057dc4ef8b60
Your Name <you@example.com>
2017-10-27 18:00:38 +0800

    Initial Commit

---
+++ sdfsdfsd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sdfsdfsd
+//
+//  Created by xuyadong on 2017/10/27.
+//  Copyright © 2017年 YiQianLiYIYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sdfsdfsd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sdfsdfsd
+//
+//  Created by xuyadong on 2017/10/27.
+//  Copyright © 2017年 YiQianLiYIYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
